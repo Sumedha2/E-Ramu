88121477a18a21a4a33c7a1a311b106127e0fe8c
Kunal Yadav <83703416+kunal-yadav-404@users.noreply.github.com>
2021-12-08 17:53:43 +0530

    Add files via upload

---
+++ menu/menu.js
@@ -0,0 +1,167 @@
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("quantity_id");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck2");
+    // Get the output text
+    var text2 = document.getElementById("quantity_id2");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text2.style.display = "block";
+    } else {
+      text2.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck3");
+    // Get the output text
+    var text3 = document.getElementById("quantity_id3");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text3.style.display = "block";
+    } else {
+      text3.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck4");
+    // Get the output text
+    var text4 = document.getElementById("quantity_id4");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text4.style.display = "block";
+    } else {
+      text4.style.display = "none";
+    }
+  }
+
+  function myFunction5() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck5");
+    // Get the output text
+    var text5 = document.getElementById("quantity_id5");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text5.style.display = "block";
+    } else {
+      text5.style.display = "none";
+    }
+  }
+
+  function myFunction6() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck6");
+    // Get the output text
+    var text3 = document.getElementById("quantity_id6");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text6.style.display = "block";
+    } else {
+      text6.style.display = "none";
+    }
+  }
+
+  function myFunction7() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck7");
+    // Get the output text
+    var text7 = document.getElementById("quantity_id7");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text7.style.display = "block";
+    } else {
+      text7.style.display = "none";
+    }
+  }
+
+  function myFunction8() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck8");
+    // Get the output text
+    var text8 = document.getElementById("quantity_id8");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text8.style.display = "block";
+    } else {
+      text8.style.display = "none";
+    }
+  }
+
+  function myFunction9() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck9");
+    // Get the output text
+    var text9 = document.getElementById("quantity_id9");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text9.style.display = "block";
+    } else {
+      text9.style.display = "none";
+    }
+  }
+
+  function myFunction10() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck10");
+    // Get the output text
+    var text10 = document.getElementById("quantity_id10");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text10.style.display = "block";
+    } else {
+      text10.style.display = "none";
+    }
+  }
+
+  function myFunction11() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck11");
+    // Get the output text
+    var text11 = document.getElementById("quantity_id11");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text11.style.display = "block";
+    } else {
+      text11.style.display = "none";
+    }
+  }
+
+  function myFunction12() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck12");
+    // Get the output text
+    var text12 = document.getElementById("quantity_id12");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text12.style.display = "block";
+    } else {
+      text12.style.display = "none";
+    }
+  }
